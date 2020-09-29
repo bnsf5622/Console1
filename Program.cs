b863683fd2f48467074843b28d312ca73f377b43
chuck francis <chuck_francis10@yahoo.com>
2020-09-29 16:08:52 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             decimal Divide(decimal a, decimal b)
+            {
+                return a / b;
+            }
+            Console.WriteLine("Forgot a semi colon!");
+            int a = 5;
+            int b = 10;
+
+            return;
+
+            int sum = a + b;
+            int[] numbers = new int[] { 3, 1, 4 };
+
+            for (int i = 0; i <= numbers.Length; i++)
+            {
+                Console.WriteLine($"The number at index {i} is {numbers[i]}.");
+            }
+        }
+    }
+}
